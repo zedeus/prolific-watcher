673dc64f8caacd5105d4a318e6e73a2d648051d8
Zed <zedeus@pm.me>
2026-03-22 22:56:06 +0100

    Update .gitignore for Prolific Pulse rename
    Rename db/binary references from prolific_watcher to prolific_pulse
    and add prolific-assistant/ to ignored directories.

--- config.go
+++ config.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	listenAddr   = ":8080"
-	sqliteDBPath = "prolific_watcher.db"
+	sqliteDBPath = "prolific_pulse.db"
 
 	internalStudiesHost                = "internal-api.prolific.com"
 	internalStudiesPath                = "/api/v1/participant/studies/"
